3f62e9fd6e83cfb39a2882db124abc4a7bddae77
Alex Alessi <alex.alessi246@gmail.com>
2026-05-13 16:04:10 +0200

    push

--- mercatino_c2b/staff_panel/forms.py
+++ mercatino_c2b/staff_panel/forms.py
@@ -1,4 +1,9 @@
 from django import forms
+from proposals.models import ItemProposal
 
 class AdminOfferForm(forms.ModelForm):
-    admin_offer = forms.PositiveIntegerField()
+    admin_offer = forms.DecimalField(min_value=0, max_digits=8, decimal_places=2, label="Offerta admin",)
+
+    class Meta:
+        model = ItemProposal
+        fields = ["admin_offer"]
--- mercatino_c2b/staff_panel/views.py
+++ mercatino_c2b/staff_panel/views.py
@@ -20,7 +20,7 @@ def staff_proposal_list_view(request):
 def staff_proposal_detail_view(request, pk):
     proposal = get_object_or_404(ItemProposal, pk=pk)
     message_form = ProposalMessageForm()
-    offer_form = AdminOfferForm()
+    offer_form = AdminOfferForm(instance=proposal)
 
     if request.method == "POST":
         action = request.POST.get("action")
@@ -36,6 +36,39 @@ def staff_proposal_detail_view(request, pk):
 
                 messages.success(request, "Messaggio inviato.")
                 return redirect("staff_proposal_detail", pk=proposal.pk)
+            
+        elif action == "send_offer":
+            offer_form = AdminOfferForm(request.POST, instance=proposal)
+
+            if offer_form.is_valid():
+                proposal = offer_form.save(commit=False)
+                proposal.status = ItemProposal.Status.OFFER_SENT
+                proposal.offer_seen_by_user = False
+                proposal.save(update_fields=["admin_offer", "status", "offer_seen_by_user", "updated_at"])
+
+                messages.success(request, "Offerta inviata correttamente.")
+                return redirect("staff_proposal_detail", pk=proposal.pk)
+        
+        elif action == "mark_negotiation":
+            proposal.status = ItemProposal.Status.NEGOTIATION
+            proposal.save(update_fields=["status"])
+
+            messages.success(request, "Proposta messa in trattativa.")
+            return redirect("staff_proposal_detail", pk=proposal.pk)
+        
+        elif action == "reject":
+            proposal.status = ItemProposal.Status.REJECTED
+            proposal.save(update_fields=["status"])
+
+            messages.success(request, "Offerta rifiutata.")
+            return redirect("staff_proposal_detail", pk=proposal.pk)
+
+        elif action == "completed":
+            proposal.status = ItemProposal.Status.COMPLETED
+            proposal.save(update_fields=["status"])
+
+            messages.success(request, "Proposta completata.")
+            return redirect("staff_proposal_detail", pk=proposal.pk)
 
     return render(request, "staff_panel/proposal_detail.html", {
     "proposal": proposal,
